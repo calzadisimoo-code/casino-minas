f037e06b75dab6d3a49d4602fecfb51296e3eb63
Camilo <calzadisimoo@gmail.com>
2026-07-20 23:59:48 -0500

    2.0

--- public/script.js
+++ public/script.js
@@ -432,30 +432,75 @@ window.onload = ()=>{
 	
 	const guardado = localStorage.getItem("usuarioGoogle");
 
-if(guardado){
-
-    usuarioGoogle = JSON.parse(guardado);
-
-    document.getElementById("loginGoogle").style.display="none";
+window.onload = ()=>{
 
     document.getElementById("juego").style.display="block";
 
-    document.getElementById("nombreUsuario").innerHTML = usuarioGoogle.name;
+    const guardado = localStorage.getItem("usuarioGoogle");
 
-    document.getElementById("foto").src = usuarioGoogle.picture;
-	document.getElementById("foto").onclick = abrirPerfil;
+    if(guardado){
 
-    socket.emit("cargarUsuario",{
+        usuarioGoogle = JSON.parse(guardado);
 
-        googleId: usuarioGoogle.sub,
+        document.getElementById("loginGoogle").style.display="none";
 
-        nombre: usuarioGoogle.name,
+        document.getElementById("nombreUsuario").innerHTML = usuarioGoogle.name;
 
-        foto: usuarioGoogle.picture
+        document.getElementById("foto").src = usuarioGoogle.picture;
+
+        document.getElementById("foto").onclick = abrirPerfil;
+
+        socket.emit("cargarUsuario",{
+
+            googleId: usuarioGoogle.sub,
+
+            nombre: usuarioGoogle.name,
+
+            foto: usuarioGoogle.picture
+
+        });
+
+    }
+
+    google.accounts.id.initialize({
+
+        client_id:"758592725329-b0d58g87fn5ihqpu3fp32b7ok6lo1ida.apps.googleusercontent.com",
+
+        callback:loginGoogle
 
     });
 
-}
+    google.accounts.id.prompt();
+
+    google.accounts.id.renderButton(
+
+        document.getElementById("loginGoogle"),
+
+        {
+
+            theme:"filled_blue",
+
+            size:"large",
+
+            width:260
+
+        }
+
+    );
+
+    dibujarTablero(
+
+        Array(25).fill({
+
+            abierta:false,
+
+            tipo:"diamante"
+
+        })
+
+    );
+
+};
 
     google.accounts.id.initialize({
 
